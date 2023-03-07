5f5568fcdc263ef8c169c61e551d4fc291461865
ListowelAdolwin <listoweladolwin@gmail.com>
2023-03-07 05:43:03 +0000

    Password reset feature added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,5 @@
+import os
+from flask_mail import Mail
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -12,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
